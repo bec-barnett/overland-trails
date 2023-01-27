ad53b4a6ea47b487f013f04d029a11686b4fd40c
bec-barnett <rebeccalucybarnett@gmail.com>
2023-01-27 15:39:05 +1100

    Setup of Fullpage.js and Shoelace UI

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Fullpage UI script, needs to be called before end of body tag in order to initialise
+var myFullpage = new fullpage('#fullpage', {
+    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff']
+});
