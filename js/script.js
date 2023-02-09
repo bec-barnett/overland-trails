baab4d419399b98eb87ab6678c0a87e05a15fbd1
bec-barnett <rebeccalucybarnett@gmail.com>
2023-02-10 09:39:04 +1100

    Updated mouse scoll handling function - now working flawlessly

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
-//initialize
-var pages = document.getElementsByClassName('slide'),
-  navLinks = document.querySelectorAll('#menu-nav a'),
-  currentPage = "0";
-
-window.addEventListener('mousewheel', function(e) {
-    e.preventDefault();
-  scrollPages(e.wheelDelta);
+// Add event listener for mousewheel 
+window.addEventListener('mousewheel', function (e) {
+  e.preventDefault();
+  scrollPages(e.wheelDelta); // handle mousewheel event
 });
 
 
 // Fullpage UI script, needs to be called before end of body tag in order to initialise
 var myFullpage = new fullpage('#fullpage', {
-    sectionsColor: ['#11190A'],
-    loopHorizontal: false,
-    anchors: ['home', 'packages', 'timeline', 'contact'],
-    menu: '#menu-nav',
-    scrollingSpeed: 1500,
+  sectionsColor: ['#11190A'],
+  loopHorizontal: false,
+  anchors: ['home', 'packages', 'timeline', 'contact'],
+  menu: '#menu-nav',
+  scrollingSpeed: 1500,
 });
 
 
@@ -53,68 +49,76 @@ function pauseStartVideo() {
 // If the video is stopped due to the user or a click on the page, remove the video
 // and replace with heroImage
 function videoFinished(e) {
-    if (currentPage == 1) {
-        fade(videoWrapper);
-        unfade(heroImage);
-        btn.innerHTML = "Play";
-    }
+  if (currentPage == 1) {
+    fade(videoWrapper);
+    unfade(heroImage);
+    btn.innerHTML = "Play";
+  }
 }
 
 
 /** Fading and Fading out Elements Functions
  * ref: StackOverflow, user "Ubu"
  * src: https://stackoverflow.com/a/6121270
- * */ 
+ * */
 function unfade(element) {
-    var op = 0.1;  // initial opacity
-    element.style.display = 'inline';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.01;
-    }, 10);
+  var op = 0.1;  // initial opacity
+  element.style.display = 'inline';
+  var timer = setInterval(function () {
+    if (op >= 1) {
+      clearInterval(timer);
+    }
+    element.style.opacity = op;
+    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    op += op * 0.01;
+  }, 10);
 }
 
 function fade(element) {
-    var op = 1;  // initial opacity
-    // element.style.display = 'inline';
-    var timer = setInterval(function () {
-        if (op <= 0){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.01;
-    }, 10);
+  var op = 1;  // initial opacity
+  // element.style.display = 'inline';
+  var timer = setInterval(function () {
+    if (op <= 0) {
+      clearInterval(timer);
+    }
+    element.style.opacity = op;
+    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    op -= op * 0.01;
+  }, 10);
 }
 
 
-
+/** 
+ * Action Listener Function for when user uses mouse scroll on page.
+ * The Delta indicates whether user scrolled up or down
+ * This can then be used to navigate the user fowards or backwards on the page
+ * As we are using horizontal scroll, this means moving from one "slide" to the next
+*/
 function scrollPages(delta) {
-  // delta indicates whether user scrolled up or down
-  // positive = up, negative = down
-  var direction = (delta > 0) ? -1 : 1;
-  var anchors = ['home', 'packages', 'timeline', 'contact'];
-  anchors.forEach((anchor, index) => {
-    if (anchor == fullpage_api.getActiveSlide().anchor) currentPage = index
-  });
-  //if (direction == 'down' && currentPage < 4) currentPage = currentPage--;
-  //if (direction == 'up' && currentPage > 1) currentPage = currentPage++;
+  var direction = (delta > 0) ? -1 : 1;  // positive = user wants to go backwards, negative = user wants to scroll forwards
+  var pageAnchors = ['home', 'about', 'packages', 'timeline', 'contact']; // Our "Page" anchors which are DIV's within index.html
+  var currentPage = 0; // A variable for storing the current page's index within pageAnchors
 
-  // alert("Active Slide is: " + fullpage_api.getActiveSlide().anchor + "Delta is: " + delta + " Direction is: " + direction + " Current Page is: " + currentPage);
-  if (currentPage == 0) {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-  //var thePlaceIwantToGoto = "#home/" + anchors[currentPage];
-  //alert(thePlaceIwantToGoto);
-  anchorIndex = currentPage + direction;
+  pageAnchors.forEach((anchor, index) => {
+    if (anchor == fullpage_api.getActiveSlide().anchor) currentPage = index; // Use fullpage's API to get the current active "slide" div
+  });
+
+  if (currentPage == 0) pauseVideo(); // If we are on the first page make sure we pause our video and reset the Play button
+  
+  var anchorIndex = currentPage + direction; // Get the index of the page anchor we want to move to
+  
+  
   if (anchorIndex == 0) {
-    window.location.href = "";
-  } else if (anchorIndex > 0 && anchorIndex < 5 ) {
-    window.location.href = "#home/" + anchors[currentPage + direction];
+    window.location.href = ""; // If its back to the home page (index 0), simply redirect to home
+  } else if (anchorIndex > 0 && anchorIndex < pageAnchors.length) {
+    window.location.href = "#main/" + pageAnchors[anchorIndex]; // redirect user to the new anchor
   }
+}
+
+/**
+ * Video to pause the home page video and reset the play button
+ */ 
+function pauseVideo() {
+  video.pause();
+  btn.innerHTML = "Play";
 }
